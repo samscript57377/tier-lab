19061ee636b9a6b70f25e1713e5552e3af9ece8f
SamScript <samuelvanmaaswaal@outlook.com>
2026-07-04 17:22:08 +0200

    Added coming-soon
    and fix bugs

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const calculatorButton = document.getElementById("calculateButton");
+const viewButton = document.getElementById("viewButton");
+const createButton = document.getElementById("createButton");
 
 calculatorButton.addEventListener("click", () => {
     window.location.href = "calculator";
+});
+
+createButton.addEventListener("click", () => {
+    window.location.href = "coming-soon";
+});
+
+viewButton.addEventListener("click", () => {
+    window.location.href = "coming-soon";
 });
